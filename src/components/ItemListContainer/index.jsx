96a951615317e0c484becd8e071aef5460e261eb
YesiDorado <doradoyesica@gmail.com>
2023-05-18 21:50:45 -0300

    preentrega

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import Title from "../Title";
 
-export const ItemListContainer = (props) => {
+const makeup = [
+    { id: 1, image: "https://www.pngegg.com/es/png-didcg"},
+    { id: 2, image: "https://www.pngegg.com/es/png-ehtlc"},
+    { id: 3, image: "https://www.pngegg.com/es/search?q=l%C3%A1piz+labial"},
+];
+
+export const ItemListContainer = ({ texto }) => {
+    const data = pedido
+    const onAdd = (quantity) => {
+        console.log(`Compraste ${quantity} unidades`)};
+    }
+
     return (
-        <Title greeting={props.texto} />
+        <>
+        <Title greeting={texto} />
+        <ItemCount initial={3} stock={5} onAdd={onAdd} />
+        <ItemList data={data} />
     );
 }
 
